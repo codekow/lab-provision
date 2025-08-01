afac653b526ad7b98ef89066889f76db983c3c16
Cory Latschkowski <koree@redhat.com>
2025-08-01 17:12:01 -0600

    update: functions

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -18,16 +18,17 @@ lab_create_vm(){
 
   sed '
       s/vm-00/'"${VM_NAME}"/g'
-      s/52:54:00:4e:e0:00/52:54:00:4e:e0:'"${VM_MAC}"/g'
+      s/52:54:00:4e:e0:00/'"${VM_MAC}"/g'
       ' "${VM_FILE}" > "scratch/${VM_NAME}".xml
 
 }
 
 lab_create_vm_set(){
+  BASE_MAC="52:54:00:4E:E0"
 
   for i in {00..10};
   do 
-    lab_create_vm vm-"$i" "$i"
+    lab_create_vm vm-"${i}" "${BASE_MAC}:$((num * 2 + 10))"
   done
 }
 
@@ -45,7 +46,7 @@ lab_uci_create_vm_dhcp(){
   uci set dhcp.@host[-1].dns='1'
 }
 
-lab_uci_setup_vm_dhcp(){
+lab_uci_setup(){
   BASE_MAC="52:54:00:4E:E0"
   BASE_IP="10.105.0"
   BASE_IP_START=61
