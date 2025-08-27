b978d700bf2bf04748da25486196b576dd6ff37f
Cory Latschkowski <koree@redhat.com>
2025-08-27 14:23:25 -0600

    fix: scripts

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-  
+
+BASE_MAC=52:54:00:4E:FF
+BASE_HOST=nab
+
 [ -d scratch ] || mkdir scratch
 
 lab_wol(){
@@ -13,7 +16,7 @@ lab_wol(){
 
 lab_create_vm(){
   VM_NAME=${1:-vm-00}
-  VM_MAC=${2:-52:54:00:4E:E0:00}
+  VM_MAC=${2:-52:54:00:4E:FF:00}
   VM_FILE=files/etc/libvirt/qemu/vm-00.xml
 
   sed '
@@ -24,7 +27,7 @@ lab_create_vm(){
 }
 
 lab_create_vm_set(){
-  BASE_MAC="52:54:00:4E:E0"
+  BASE_MAC=${BASE_MAC:-52:54:00:4E:FF}
 
   for i in {00..15}
   do
@@ -36,7 +39,7 @@ lab_create_vm_set(){
 
 lab_create_bmh(){
   VM_NAME=${1:-vm-00}
-  VM_MAC=${2:-52:54:00:4E:E0:00}
+  VM_MAC=${2:-52:54:00:4E:FF:00}
   VM_HOST=${3:-nuc10-01}
   VM_FILE=files/ocp/bmh-00.yaml
 
@@ -49,21 +52,21 @@ lab_create_bmh(){
 }
 
 lab_create_bmh_set(){
-  BASE_MAC="52:54:00:4E:E0"
-  BASE_HOST=nuc10
+  BASE_MAC=${BASE_MAC:-52:54:00:4E:FF}
+  BASE_HOST=${BASE_HOST:-nuc10}
 
-  for i in {03..15}
+  for i in {00..15}
   do
     # kludge for sh
     num=${i#0}
-    lab_create_vm vm-"${i}" "${BASE_MAC}:$((num * 2 + 10))" "${BASE_HOST}-${i}"
+    lab_create_bmh vm-"${i}" "${BASE_MAC}:$((num * 2 + 10))" "${BASE_HOST}-${i}"
   done
 }
 
 
 lab_uci_create_vm_dhcp(){
   VM_NAME=${1:-vm-00}
-  VM_MAC=${2:-52:54:00:4E:E0:00}
+  VM_MAC=${2:-52:54:00:4E:FF:00}
   VM_IP=${3:-10.105.0.100}
   VM_LEASE=${4:-5m}
 
@@ -76,7 +79,7 @@ lab_uci_create_vm_dhcp(){
 }
 
 lab_uci_setup(){
-  BASE_MAC="52:54:00:4E:E0"
+  BASE_MAC="52:54:00:4E:FF"
   BASE_IP="10.105.0"
   BASE_IP_START=61
   BASE_START=00
